c318228cea03c000d577150777fcab20193c8142
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-02-22 02:59:52 +0300

    Made the player movement smoother.

---
+++ drawMap.c
@@ -0,0 +1,202 @@
+#include "newMaze.h"
+
+int cubeUnit = 32;
+
+void drawMap()
+{
+    int x, y;
+    SDL_Rect cube;
+    x = 0;
+    y = 0;
+
+
+    for (int yCor = 0; yCor < mapHeight; yCor++)
+    {
+        for (int xCor = 0; xCor < mapWidth; xCor++)
+        {
+            cube.x = x;
+            cube.y = y;
+            cube.w = cubeUnit;
+            cube.h = cubeUnit;
+
+            if (gameMap[yCor][xCor] != 0)
+            {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255,255);
+                SDL_RenderDrawRect(renderer, &cube);
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0,255);
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderDrawRect(renderer, &cube);
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255,255);
+            }
+            
+            cube.x = x + 1;
+            cube.y = y + 1;
+            cube.w = cubeUnit - 1;
+            cube.h = cubeUnit - 1;
+
+            SDL_RenderFillRect(renderer, &cube);
+
+            x += cubeUnit;
+            // printf("Cube.x = %d, cube.y = %d, cube.w = %d, cube.h = %d\n", cube.x, cube.y, cube.w, cube.h);
+        }
+        x = 0;
+        y += cubeUnit;
+        // printf("Y %d\n", yCor);
+    }
+}
+
+void drawPlayer()
+{
+    SDL_Rect player = {(cubeUnit * playerX) - 3 , (cubeUnit * playerY) - 3, 6, 6};
+
+    SDL_RenderFillRect(renderer, &player);
+}
+
+point getFinalPoint(double rayDirX, double rayDirY)
+{
+    // length of ray from player position to first intersection.
+    double sideDistX, sideDistY, intX, intY;
+
+    int stepX, stepY, hit, side;
+
+    point intersect;
+    vector normVector;
+
+    // Grid cell we are at.
+    int mapX = playerX; // truncates the playerX position.
+    int mapY = playerY;
+
+    // length of ray each step along the x or y axis.
+    double deltaDistX = (rayDirX == 0) ? INFINITY : sqrt(1 + ((rayDirY * rayDirY)/(rayDirX*rayDirX)));
+    double deltaDistY = (rayDirY == 0) ? INFINITY : sqrt(1 + ((rayDirX * rayDirX)/(rayDirY*rayDirY)));
+
+    hit = 0; // zero if collision has not occurred, else 1.
+
+    // getting first distance and steps.
+    if (rayDirX < 0)
+    {
+        stepX = -1; // left side.
+        sideDistX = (playerX - mapX) * deltaDistX;
+    }
+    else
+    {
+        stepX = 1; // right side.
+        sideDistX = ((mapX + 1.0) - playerX) * deltaDistX;
+    }
+
+    if (rayDirY < 0)
+    {
+        stepY = -1; // upwards.
+        sideDistY = (playerY - mapY) * deltaDistY;
+    }
+    else
+    {
+        stepY = 1; // downwards.
+        sideDistY = ((mapY + 1.0) - playerY) * deltaDistY;
+    }
+
+    // Check for a hit.
+    while (hit == 0 && (mapX + 1) < mapWidth && (mapY + 1) < mapHeight && (mapX - 1) >= 0 && (mapY - 1) >= 0)
+    {
+        // choosing which axis intersection to follow - x or y.
+        if (sideDistX < sideDistY)
+        {
+            sideDistX += deltaDistX;
+            mapX += stepX;
+            side = 0; // meaning hit a vertical line i.e followed x axis.
+        }
+        else
+        {
+            sideDistY += deltaDistY;
+            mapY += stepY;
+            side = 1; // hit a horizontal line.
+        }
+
+        // Checking for a hit.
+        if (gameMap[mapY][mapX] > 0)
+            hit = 1;
+    }
+
+    if (hit == 1)
+    {
+        normVector = normalizeVector(rayDirX, rayDirY);
+        if (side == 0)
+        {
+            intX = playerX + normVector.x * (sideDistX - deltaDistX);
+            intY = playerY + normVector.y * (sideDistX - deltaDistX);
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        }
+        else
+        {
+            intX = playerX + normVector.x * (sideDistY - deltaDistY);
+            intY = playerY + normVector.y * (sideDistY - deltaDistY);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        }
+    }
+    else
+        printf("No hit!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+
+    intersect.x = intX;
+    intersect.y = intY;
+    return intersect;
+}
+
+void drawLineOfSight()
+{
+    double rayDirX, rayDirY;
+    point intersect;
+    vector ray;
+
+    for (int x = 0; x < screenWidth; x++)
+    {
+        ray = getRayDir(x);
+        rayDirX = ray.x;
+        rayDirY = ray.y;
+
+        intersect = getFinalPoint(rayDirX, rayDirY);
+        SDL_RenderDrawLine(renderer, (playerX * cubeUnit), (playerY * cubeUnit), (intersect.x * cubeUnit), (intersect.y * cubeUnit));
+    }
+}
+
+int main()
+{
+	SDL_Event e;
+	bool quit = false;
+    // coordinates wall;
+    // int keys[256] = {0};
+    //const Uint8 *keyboard_state_array = SDL_GetKeyboardState(NULL);
+
+	if (!init())
+	{
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		while( quit == false )
+		{
+			while( SDL_PollEvent( &e ) )
+			{
+				if( e.type == SDL_QUIT )
+					quit = true;
+                movement(e);
+            }
+            move();
+
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white
+    		SDL_RenderClear(renderer);
+
+			drawMap();
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            drawPlayer();
+            drawLineOfSight();
+            //printf("PlayerX: %d PlayerY: %d\n", playerX, playerY);
+            SDL_RenderPresent(renderer);
+
+		}
+	}
+
+	closeDown();
+}
---
+++ movement.c
@@ -0,0 +1,101 @@
+#include "newMaze.h"
+
+void movement(SDL_Event event)
+{
+    player.moveSpeed = 0;
+    player.rotSpeed = 0;
+    // printf("Move speed id %f\n", moveSpeed);
+
+    if (event.type == SDL_KEYDOWN)
+    {
+        switch (event.key.keysym.sym)
+        {
+        // Rotation
+        case SDLK_LEFT:
+            player.rotSpeed -= degToRad(1.0);
+            break;
+        case SDLK_RIGHT:
+            player.rotSpeed += degToRad(1.0);
+            break;
+        // Movement
+        case SDLK_UP:
+            player.moveSpeed += 0.2;
+            player.forward = 1;
+            break;
+        case SDLK_DOWN:
+            player.moveSpeed -= 0.2;
+            player.forward = 0;
+            break;
+        default:
+            break;
+        }
+    }
+    else if(event.type == SDL_KEYUP)
+    {
+        switch (event.key.keysym.sym)
+        {
+        // Rotation
+        case SDLK_LEFT:
+            player.rotSpeed = 0;
+            break;
+        case SDLK_RIGHT:
+            player.rotSpeed = 0;
+            break;
+        // Movement
+        case SDLK_UP:
+            player.moveSpeed = 0;
+            break;
+        case SDLK_DOWN:
+            player.moveSpeed = 0;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void moveForward()
+{
+    if (player.moveSpeed != 0)
+    {
+        if (player.forward == 1)
+        {
+            if (gameMap[(int)(playerY)][(int)(playerX + (dirX * (player.moveSpeed + 1)))] == 0)
+                playerX += (dirX * player.moveSpeed);
+            if (gameMap[(int)(playerY + (dirY * (player.moveSpeed + 1)))][(int)(playerX)] == 0)
+                playerY += (dirY * player.moveSpeed);
+        }
+        else
+        {
+            if (gameMap[(int)(playerY)][(int)(playerX + (dirX * (player.moveSpeed - 1)))] == 0)
+                playerX += (dirX * player.moveSpeed);
+            if (gameMap[(int)(playerY + (dirY * (player.moveSpeed - 1)))][(int)(playerX)] == 0)
+                playerY += (dirY * player.moveSpeed);
+        }
+    }
+}
+
+
+void moveSideWays()
+{
+    // Use the rotation matrix.
+    if (player.rotSpeed != 0)
+    {
+    double oldDirX = dirX;
+    dirX = (dirX * cos(player.rotSpeed)) - (dirY * sin(player.rotSpeed)); // -ve player.rotSpeed is because it's rotation to the right.
+    dirY = (oldDirX * sin(player.rotSpeed)) + (dirY * cos(player.rotSpeed));
+    
+    // Rotating the plane.
+    double oldPlaneX = planeX;
+    planeX = (planeX * cos(player.rotSpeed)) - (planeY * sin(player.rotSpeed));
+    planeY = (oldPlaneX * sin(player.rotSpeed)) + (planeY * cos(player.rotSpeed));
+    }
+}
+
+void move()
+{
+    if (player.moveSpeed)
+        moveForward();
+    if (player.rotSpeed)
+        moveSideWays();
+}
